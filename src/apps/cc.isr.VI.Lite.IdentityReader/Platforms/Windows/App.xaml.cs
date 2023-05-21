2b3558e9626a0f3ac35c8bda87504713432cdd15
David Hary <791370+ATECoder@users.noreply.github.com>
2023-05-20 21:12:54 -0700

    Cleanup and unify project file and documentation.

--- src/apps/cc.isr.VI.Lite.IdentityReader/Platforms/Windows/App.xaml.cs
+++ src/apps/cc.isr.VI.Lite.IdentityReader/Platforms/Windows/App.xaml.cs
@@ -8,10 +8,10 @@ namespace cc.isr.VI.Lite.IdentityReader.WinUI;
 /// </summary>
 public partial class App : MauiWinUIApplication
 {
-	/// <summary>
-	/// Initializes the singleton application object.  This is the first line of authored code
-	/// executed, and as such is the logical equivalent of main() or WinMain().
-	/// </summary>
+    /// <summary>
+    /// Initializes the singleton application object.  This is the first line of authored code
+    /// executed, and as such is the logical equivalent of main() or WinMain().
+    /// </summary>
 	public App()
 	{
 		this.InitializeComponent();
